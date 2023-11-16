d2bd2a1faab1cd7862db8c30e26db31ce8f22351
pavkata2503 <pavkatapetrov@gmail.com>
2023-11-16 13:39:34 +0200

    Done

---
+++ Cinema/Cinema/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using Cinema.Data;
+using Cinema.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public MovieController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Movie movie) 
+        {
+            context.Movies.Add(movie);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
